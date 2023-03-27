13923c8447483b15e8bba5c66a966ada3f025892
denisaiacob <iacobdenisaalexandra49@gmail.com>
2023-03-27 20:55:19 +0300

    tema 3

--- Tema 3/main.py
+++ Tema 3/main.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-eps = 10 ** -6
 
 
 def ex1(A, s, n):
-    b = np.zeros((n, 1))
+    b = np.zeros(n)
     for i in range(0, n):
         for j in range(0, n):
             b[i] = b[i] + s[j] * A[i, j]
@@ -12,8 +11,7 @@ def ex1(A, s, n):
 
 
 def ex2(A, b, n):
-    In = np.ones((n))
-    q = np.zeros((n, n))
+    q = np.zeros((n,n))
     for i in range(0, n):
         q[i, i] = 1
     for r in range(0, n - 1):
@@ -57,58 +55,38 @@ def ex2(A, b, n):
     return q
 
 
-def ex3QR(Ainit, b):
-    q, r = np.linalg.qr(Ainit)
-    print(q)
-    print(r)
-    x = np.zeros((n, 1))
-    z = np.zeros(((n, 1)))
-    for i in range(0, n):
-        z[i] = b[i]
-        for j in range(0, i):
-            z[i] = z[i] - r[i, j] * z[j]
-    for i in range(n - 1, -1, -1):
-        x[i] = z[i]
-        x[n - 1] = z[n - 1] / r[n - 1, n - 1]
-        if i != n - 1:
-            for j in range(n - 1, i, -1):
-                x[i] = (x[i] - r[i, j] * x[j])
+def ex3QR(A, b):
+    Q, R = np.linalg.qr(A)
+    x = np.linalg.solve(np.dot(Q, R), b)
     return x
 
 
 def ex3Householder(A, b):
-    x = np.zeros((n, 1))
-    z = np.zeros(((n, 1)))
-    for i in range(0, n):
-        z[i] = b[i]
-        for j in range(0, i):
-            z[i] = z[i] - A[i, j] * z[j]
+    x = np.zeros(n)
     for i in range(n - 1, -1, -1):
-        x[i] = z[i]
-        x[n - 1] = z[n - 1] / A[n - 1, n - 1]
-        if i != n - 1:
-            for j in range(n - 1, i, -1):
-                x[i] = (x[i] - A[i, j] * x[j])
+        sum = 0
+        for j in range(i+1, n):
+            sum += A[i, j] * x[j]
+        x[i] = (b[i] - sum)/A[i, i]
     return x
 
 
-def ex41(A, b, x):
-    # norma = 0
-    # Y = np.zeros(n)
-    # for i in range(n):
-    #     for j in range(n):
-    #         Y[i] += A[i, j] * x[j]
-    #     Y[i] -= b[i]
-    #     norma += Y[i] ** 2
-    # norma = np.sqrt(norma)
-    norma = np.linalg.norm(np.dot(A, x, b))
+def ex4a(A, b, x):
+    norma = 0
+    Y = np.zeros(n)
+    for i in range(n):
+        for j in range(n):
+            Y[i] += A[i, j] * x[j]
+        Y[i] -= b[i]
+        norma += Y[i] ** 2
+    norma = np.sqrt(norma)
     if norma < eps:
-        print("Norma este:", norma)
+        print("Norma pentru ||A^init * x - b^init|| este:", norma)
     else:
-        print("Norma nu este mai mica decat epsilon")
+        print("Norma este:", norma)
 
 
-def ex42(s, x):
+def ex4b(x, s):
     norma1 = 0
     for i in range(n):
         x[i] = x[i] - s[i]
@@ -117,10 +95,9 @@ def ex42(s, x):
     norma2 = np.linalg.norm(s)
     norma = norma1 / norma2
     if norma < eps:
-        print("Norma este:", norma)
+        print("Norma pentru ||x - s|| / ||s|| este:", norma)
     else:
-        print("Norma nu este mai mica decat epsilon")
-
+        print("Norma este:", norma)
 
 def ex5(A, q):
     x = np.zeros((n, 1))
@@ -140,27 +117,39 @@ def ex5(A, q):
 
 if __name__ == '__main__':
     eps = 10 ** -6
-    # Ainit = np.array(([0, 0, 4], [1, 2, 3], [0, 1, 2]))
-    Ainit = np.random.randint(0, 2000, size=(10, 10))
+
+    # Ainit = np.array([[0, 0, 4], [1, 2, 3], [0, 1, 2]])
+    Ainit = np.random.randint(0, 100, size=(6, 6))
+    while np.linalg.det(Ainit) == 0:
+        Ainit = np.random.randint(0, 100, size=(6, 6))
     A = Ainit.copy()
-    # s = np.array(([3, 2, 1]))
-    s = np.random.randint(0, 2000, size=(10, 1))
-    n, n = A.shape
+    # s = np.array([3, 2, 1])
+    s = np.random.randint(0, 100, size=(10, 1))
+    n = A.shape[0]
     b = ex1(A, s, n)
+    binit = b.copy()
+    print("binit este:", binit)
+    AinvBibl = np.linalg.inv(A)
     q = ex2(A, b, n)
-    xQR = ex3QR(Ainit, b)
-    xHouseholder = ex3Householder(A, b)
+    print("R este:", A)
+    print("Q este:", q.T)
+    xQR = ex3QR(Ainit, binit)
+    xHouseholder = ex3Householder(A,b)
+    print("xHouseHolder este: ", xHouseholder)
+    print("xQR este: ", xQR)
     print("Norma ex 3 este:", np.linalg.norm(xQR - xHouseholder))
 
-    ex41(Ainit, b, xHouseholder)
-    ex41(Ainit, b, xQR)
-    ex42(s, xHouseholder)
-    ex42(s, xQR)
+    ex4a(Ainit, binit, xQR)
+    ex4a(Ainit, binit, xHouseholder)
+    ex4b(xQR, s)
+    ex4b(xHouseholder, s)
+
     qinv = np.linalg.inv(q)
-    AinvHouseholder = np.empty([n, 1])
+    AinvHouseholder = np.empty([n, n])
     for i in range(0, n):
-        Ainv = np.append(AinvHouseholder, ex5(A, qinv[i]), axis=1)
-    AinvHouseholder = np.delete(Ainv, np.s_[0:1], axis=1)
-    AinvBibl = np.linalg.inv(A)
+        AinvHouseholder = np.append(AinvHouseholder, ex5(A, qinv[i]), axis=1)
+    AinvHouseholder = np.delete(AinvHouseholder, np.s_[0:n], axis=1)
+    print(AinvHouseholder)
+    print(AinvBibl)
     norma = np.linalg.norm(AinvHouseholder - AinvBibl)
     print("Norma ex 5:", norma)
